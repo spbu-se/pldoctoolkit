357047d23df6fb71355ac22d64ac5167115964d0
ks.yakovlev <ks.yakovlev@5f699344-e928-0410-99ec-830c0de225ea>
2007-02-25 22:07:25 +0000

    Issue 2: Add Wizards for files creation

---
+++ trunk/publisher/src/org/spbu/pldoctoolkit/wizards/NewFamilyWizard.java
@@ -0,0 +1,25 @@
+package org.spbu.pldoctoolkit.wizards;
+
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+
+public class NewFamilyWizard extends Wizard implements INewWizard {
+
+	public NewFamilyWizard() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean performFinish() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
