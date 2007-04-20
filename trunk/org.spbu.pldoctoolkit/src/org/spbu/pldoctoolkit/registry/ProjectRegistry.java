99d84b7fba604111686b1b72183d6c955c8c23a8
ks.yakovlev <ks.yakovlev@5f699344-e928-0410-99ec-830c0de225ea>
2007-04-20 22:36:01 +0000

    Implemented a resource registry

--- trunk/org.spbu.pldoctoolkit/src/org/spbu/pldoctoolkit/registry/ProjectRegistry.java
+++ trunk/org.spbu.pldoctoolkit/src/org/spbu/pldoctoolkit/registry/ProjectRegistry.java
@@ -1,6 +1,13 @@
 package org.spbu.pldoctoolkit.registry;
 
+import java.util.List;
+
+import org.eclipse.core.resources.IFile;
 
 public interface ProjectRegistry {
 	public RegisteredLocation getRegisteredLocation(String uri);
+	public String getContext(IFile file);
+	public List<RegisteredLocation> findForType(String type);
+	public List<RegisteredLocation> findForFile(IFile file);
+	public List<RegisteredLocation> findAll();
 }
