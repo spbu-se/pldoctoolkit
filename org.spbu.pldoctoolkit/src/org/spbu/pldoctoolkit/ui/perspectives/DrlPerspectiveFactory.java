6404c9e7d4f0d7236fca4bf9bccff3a50952ec5b
ks.yakovlev <ks.yakovlev@5f699344-e928-0410-99ec-830c0de225ea>
2007-04-22 01:49:41 +0000

    Tuned perspective

--- org.spbu.pldoctoolkit/src/org/spbu/pldoctoolkit/ui/perspectives/DrlPerspectiveFactory.java
+++ org.spbu.pldoctoolkit/src/org/spbu/pldoctoolkit/ui/perspectives/DrlPerspectiveFactory.java
@@ -15,6 +15,7 @@ public class DrlPerspectiveFactory implements IPerspectiveFactory {
 		// Add "new wizards".
 		layout.addNewWizardShortcut("org.eclipse.ui.wizards.new.folder");
 		layout.addNewWizardShortcut("org.eclipse.ui.wizards.new.file");
+		layout.addNewWizardShortcut("org.spbu.pldoctoolkit.text.wizards.new.drlfile");
 		layout.addNewWizardShortcut("org.spbu.pldoctoolkit.graph.presentation.DrlModelWizardID");
 		layout.addNewWizardShortcut("org.spbu.pldoctoolkit.graph.diagram.infproduct.part.DrlModelCreationWizardID");
 		layout.addNewWizardShortcut("org.spbu.pldoctoolkit.graph.diagram.productline.part.DrlModelCreationWizardID");
@@ -27,16 +28,17 @@ public class DrlPerspectiveFactory implements IPerspectiveFactory {
 		layout.addShowViewShortcut(IPageLayout.ID_TASK_LIST);
 
 		layout.addPerspectiveShortcut("org.eclipse.ui.resourcePerspective");
+		layout.addPerspectiveShortcut("org.spbu.pldoctoolkit.ui.drlPerspective");
 	}
 
 	public void defineLayout(IPageLayout layout) {
 		String editorArea = layout.getEditorArea();
 
 		layout.addView(IPageLayout.ID_RES_NAV, IPageLayout.LEFT, 0.26f, editorArea);
-		layout.addView(IPageLayout.ID_OUTLINE, IPageLayout.BOTTOM,
-				IPageLayout.DEFAULT_VIEW_RATIO, IPageLayout.ID_RES_NAV);
+//		layout.addView(IPageLayout.ID_OUTLINE, IPageLayout.BOTTOM,
+//				IPageLayout.DEFAULT_VIEW_RATIO, IPageLayout.ID_RES_NAV);
 
-		IFolderLayout bottomFolder = layout.createFolder("bottom", IPageLayout.BOTTOM, 0.66f, editorArea);
+		IFolderLayout bottomFolder = layout.createFolder("bottom", IPageLayout.BOTTOM, 0.75f, editorArea);
 		bottomFolder.addView(IPageLayout.ID_PROP_SHEET);
 		bottomFolder.addView("org.eclipse.pde.runtime.LogView");
 	}
