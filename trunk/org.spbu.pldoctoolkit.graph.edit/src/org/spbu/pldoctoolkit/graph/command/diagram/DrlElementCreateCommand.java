5615b5ff704060fcac3a15aa07f70769410ccbed
alexeysem <alexeysem@5f699344-e928-0410-99ec-830c0de225ea>
2007-05-06 17:59:06 +0000

    - fixed productline diagram: now products can be created - added create/destroy commands for diagrams - changed model: removed 2 unnecessary references from ProductLine, regenerated model code - still no properties for diagram elements in properties view

---
+++ trunk/org.spbu.pldoctoolkit.graph.edit/src/org/spbu/pldoctoolkit/graph/command/diagram/DrlElementCreateCommand.java
@@ -0,0 +1,56 @@
+package org.spbu.pldoctoolkit.graph.command.diagram;
+
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.gmf.runtime.emf.type.core.commands.CreateElementCommand;
+import org.eclipse.gmf.runtime.emf.type.core.requests.CreateElementRequest;
+import org.spbu.pldoctoolkit.graph.DrlElement;
+import org.w3c.dom.Node;
+
+public class DrlElementCreateCommand extends CreateElementCommand {
+
+	public DrlElementCreateCommand(CreateElementRequest request) {
+		super(request);
+	}
+
+	@Override
+	protected EObject doDefaultElementCreation() {
+		DrlElement newElement = (DrlElement) super.doDefaultElementCreation();
+		
+		if(newElement != null) {
+			newElement.initializeNode(getContainmentFeature());
+		}
+		
+		DrlElement container = (DrlElement) newElement.eContainer();
+		container.getNode().appendChild(newElement.getNode());
+		
+		return newElement;
+	}
+
+	@Override
+	protected IStatus doRedo(IProgressMonitor monitor, IAdaptable info)
+			throws ExecutionException {
+				IStatus result = super.doRedo(monitor, info);
+				
+				DrlElement newElement = (DrlElement) getNewElement();
+				DrlElement container = (DrlElement) newElement.eContainer();
+				container.getNode().appendChild(newElement.getNode());
+				
+				return result;
+			}
+
+	@Override
+	protected IStatus doUndo(IProgressMonitor monitor, IAdaptable info)
+			throws ExecutionException {
+				DrlElement newElement = (DrlElement) getNewElement();
+				Node elementNode = newElement.getNode();
+				
+				elementNode.getParentNode().removeChild(elementNode);
+				
+				return super.doUndo(monitor, info);
+			}
+
+}
