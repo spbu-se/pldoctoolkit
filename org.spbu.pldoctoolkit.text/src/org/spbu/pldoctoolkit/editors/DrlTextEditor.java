5c1c7224d2782c4a7c28c9b16c1e4e47670c2982
ks.yakovlev <ks.yakovlev@5f699344-e928-0410-99ec-830c0de225ea>
2007-06-12 20:27:56 +0000

    Added validate feature for perform full validation without transforming into some format for publication

--- org.spbu.pldoctoolkit.text/src/org/spbu/pldoctoolkit/editors/DrlTextEditor.java
+++ org.spbu.pldoctoolkit.text/src/org/spbu/pldoctoolkit/editors/DrlTextEditor.java
@@ -5,11 +5,13 @@ import org.spbu.pldoctoolkit.DrlPublisherPlugin;
 import org.spbu.pldoctoolkit.actions.BasicExportAction;
 import org.spbu.pldoctoolkit.actions.PdfExportAction;
 import org.spbu.pldoctoolkit.actions.ValidateDrlAction;
+import org.spbu.pldoctoolkit.actions.ValidateDrlOnSaveAction;
 
 public class DrlTextEditor extends TextEditor {
 	public static final String XML_PARTITIONING = "__drl_partitioning";
 	
-	public static final String VALIDATE_DRL = "validate_action";
+	public static final String VALIDATE_DRL_ON_SAVE = "validate_drl_on_save";
+	public static final String VALIDATE_DRL = "validate_drl";
 	public static final String EXPORT_TO_HTML = "export_to_html";
 	public static final String EXPORT_TO_PDF = "export_to_pdf";
 	
@@ -35,12 +37,18 @@ public class DrlTextEditor extends TextEditor {
 	@Override
 	protected void editorSaved() {
 		super.editorSaved();
-		this.getAction(VALIDATE_DRL).run();
+		this.getAction(VALIDATE_DRL_ON_SAVE).run();
 	}
 
 	@Override
 	protected void createActions() {
 		super.createActions();
+		try {
+			setAction(VALIDATE_DRL_ON_SAVE, new ValidateDrlOnSaveAction(this));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 		try {
 			setAction(VALIDATE_DRL, new ValidateDrlAction(this));
 		} catch (Exception e) {
@@ -48,7 +56,7 @@ public class DrlTextEditor extends TextEditor {
 		}
 		
 		try {
-			setAction(EXPORT_TO_HTML, new BasicExportAction(this, DrlPublisherPlugin.getURL("xsl/docbook/html/docbook.xsl"),"HTML", "html"));
+			setAction(EXPORT_TO_HTML, new BasicExportAction(this, DrlPublisherPlugin.getURL("xsl/docbook/html/docbook.xsl"), "Export to HTML", "HTML", "html"));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
