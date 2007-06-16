2429931c447f065a7770a5fcdec2a96a1c55c31a
alexeysem <alexeysem@5f699344-e928-0410-99ec-830c0de225ea>
2007-06-16 05:44:43 +0000

    translation fixes; diagram create wizard disabled; working on getting layout bugs fixed

--- org.spbu.pldoctoolkit.graph.diagram.infproduct/src/org/spbu/pldoctoolkit/graph/diagram/infproduct/edit/parts/DocumentationCoreEditPart.java
+++ org.spbu.pldoctoolkit.graph.diagram.infproduct/src/org/spbu/pldoctoolkit/graph/diagram/infproduct/edit/parts/DocumentationCoreEditPart.java
@@ -1,29 +1,26 @@
 package org.spbu.pldoctoolkit.graph.diagram.infproduct.edit.parts;
 
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 
 import org.eclipse.core.runtime.IAdaptable;
-import org.eclipse.emf.ecore.EObject;
+import org.eclipse.gef.EditPart;
 import org.eclipse.gef.EditPolicy;
 import org.eclipse.gef.Request;
 import org.eclipse.gef.commands.Command;
-import org.eclipse.gef.commands.CompoundCommand;
+import org.eclipse.gef.editpolicies.FlowLayoutEditPolicy;
 import org.eclipse.gef.editpolicies.LayoutEditPolicy;
+import org.eclipse.gef.editpolicies.NonResizableEditPolicy;
 import org.eclipse.gef.requests.CreateRequest;
-import org.eclipse.gef.requests.GroupRequest;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.DiagramEditPart;
-import org.eclipse.gmf.runtime.diagram.ui.editparts.GraphicalEditPart;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.IBorderItemEditPart;
+import org.eclipse.gmf.runtime.diagram.ui.editpolicies.BorderItemSelectionEditPolicy;
 import org.eclipse.gmf.runtime.diagram.ui.editpolicies.ContainerEditPolicy;
 import org.eclipse.gmf.runtime.diagram.ui.editpolicies.EditPolicyRoles;
 import org.eclipse.gmf.runtime.diagram.ui.editpolicies.XYLayoutEditPolicy;
 import org.eclipse.gmf.runtime.diagram.ui.requests.ArrangeRequest;
-import org.eclipse.gmf.runtime.diagram.ui.requests.CreateViewAndElementRequest;
 import org.eclipse.gmf.runtime.diagram.ui.requests.CreateViewRequest;
 import org.eclipse.gmf.runtime.diagram.ui.requests.RequestConstants;
-import org.eclipse.gmf.runtime.emf.core.util.EObjectAdapter;
 import org.eclipse.gmf.runtime.notation.View;
 import org.spbu.pldoctoolkit.graph.diagram.infproduct.edit.policies.DocumentationCoreCanonicalEditPolicy;
 import org.spbu.pldoctoolkit.graph.diagram.infproduct.edit.policies.DocumentationCoreItemSemanticEditPolicy;
@@ -78,8 +75,31 @@ public class DocumentationCoreEditPart extends DiagramEditPart {
 				return layoutProvider.layoutLayoutNodes(nodes, offsetFromBoundingBox, layoutHint);
 			}
 		});
+		
+		installEditPolicy(EditPolicy.LAYOUT_ROLE, createLayoutEditPolicy());
 	}
 
+	protected LayoutEditPolicy createLayoutEditPolicy() {
+		LayoutEditPolicy lep = new XYLayoutEditPolicy() {
+
+			protected EditPolicy createChildEditPolicy(EditPart child) {
+				if (child instanceof IBorderItemEditPart) {
+					return new BorderItemSelectionEditPolicy();
+				}
+				EditPolicy result = child.getEditPolicy(EditPolicy.PRIMARY_DRAG_ROLE);
+				if (result == null) {
+					result = new NonResizableEditPolicy();
+				}
+				return result;
+			}
+
+//			protected Command getCreateCommand(CreateRequest request) {
+//				return null;
+//			}
+		};
+		return lep;
+	}
+	
 	/**
 	 * Performs relayout on create, move children or resize children request.<p>
 	 *
@@ -88,17 +108,35 @@ public class DocumentationCoreEditPart extends DiagramEditPart {
 	 */
 	@Override
 	public Command getCommand(Request request) {
+		System.out.println("req: " + request);
+		
+//		if(CreateViewRequest.class.isAssignableFrom(request.getClass())) {
+//			System.out.println("is craete view!");
+//		}
+		
 		Command command = super.getCommand(request);
-		if (request.getType().equals(REQ_CREATE) || request.getType().equals(REQ_MOVE_CHILDREN) || request.getType().equals(REQ_RESIZE_CHILDREN)) {
+		if (request.getType().equals(REQ_CREATE) 
+				|| request.getType().equals(REQ_MOVE_CHILDREN) 
+				|| request.getType().equals(REQ_RESIZE_CHILDREN)
+//				|| isCreateViewRequest(request)
+				) {
+			System.out.println("here!!" + (isCreateViewRequest(request)? " and view!" : ""));
 			ArrangeRequest layoutRequest = new ArrangeRequest(RequestConstants.REQ_ARRANGE_DEFERRED);
 			List editParts = getChildren();
 			layoutRequest.setViewAdaptersToArrange(new ArrayList(editParts));
+//			if(isCreateViewRequest(request)) {
+//				layoutRequest.setViewAdaptersToArrange(((CreateViewRequest)request).getViewDescriptors());
+//			}
 			Command layoutCommand = super.getCommand(layoutRequest);
 			if (layoutCommand != null && command != null) {
 				command = command.chain(layoutCommand);
 			}
 		}
 		return command;
+	}
+
+	private boolean isCreateViewRequest(Request request) {
+		return CreateViewRequest.class.isAssignableFrom(request.getClass());
 	}
 	
 
