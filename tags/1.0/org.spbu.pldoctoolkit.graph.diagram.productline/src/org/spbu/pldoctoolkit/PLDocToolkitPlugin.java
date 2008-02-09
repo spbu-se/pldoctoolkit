038a9cc98306658a3fc04a0a35c4f8e467cffcf9
alexeysem <alexeysem@5f699344-e928-0410-99ec-830c0de225ea>
2008-02-09 13:19:09 +0000

    tagged plugins for version 1.0 of the product
    version 1.0 has been released 19th June

---
+++ tags/1.0/org.spbu.pldoctoolkit.graph.diagram.productline/src/org/spbu/pldoctoolkit/PLDocToolkitPlugin.java
@@ -0,0 +1,61 @@
+package org.spbu.pldoctoolkit;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+import org.spbu.pldoctoolkit.registry.ProjectRegistry;
+import org.spbu.pldoctoolkit.registry.WorkspaceRegistryIndex;
+
+public class PLDocToolkitPlugin extends AbstractUIPlugin {
+	
+	// The plug-in ID
+	public static final String PLUGIN_ID = "org.spbu.pldoctoolkit";
+
+	public static final String DRL_FILE_EXTENSION = "drl";
+
+	// The shared instance
+	private static PLDocToolkitPlugin plugin;
+	
+	private final WorkspaceRegistryIndex registryIndex;
+	
+	public PLDocToolkitPlugin() throws CoreException {
+		plugin = this;
+		registryIndex = new WorkspaceRegistryIndex(ResourcesPlugin.getWorkspace());
+	}
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		super.stop(context);
+		plugin = null;
+	}
+	
+	public static ProjectRegistry getRegistry(String projectName) {
+		return plugin.registryIndex.getRegistry(projectName);
+	}
+	
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static PLDocToolkitPlugin getDefault() {
+		return plugin;
+	}
+	
+	public static URL getURL(String relativePath) {
+		try {
+			return new URL(getDefault().getBundle().getEntry("/"), relativePath);
+		} catch (MalformedURLException e) {
+			return null;
+		}
+	}
+}
