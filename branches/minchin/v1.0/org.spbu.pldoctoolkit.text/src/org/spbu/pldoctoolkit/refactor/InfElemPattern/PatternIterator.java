507c4fdfcb24d7dabaed950bcfa6c8339de805fd
Leonid.Minchin <Leonid.Minchin@5f699344-e928-0410-99ec-830c0de225ea>
2008-05-12 05:49:07 +0000

    1. Начало сопоставления с шаблоном 2. Диалог для проверки удаления лишних знаков

---
+++ branches/minchin/v1.0/org.spbu.pldoctoolkit.text/src/org/spbu/pldoctoolkit/refactor/InfElemPattern/PatternIterator.java
@@ -0,0 +1,68 @@
+package org.spbu.pldoctoolkit.refactor.InfElemPattern;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Stack;
+
+import org.spbu.pldoctoolkit.refactor.Couple;
+
+public class PatternIterator {
+	private PatternElement pattern;
+	//private PatternElement curElem;
+	private LinkedList<PatternElement> stack = new LinkedList<PatternElement>();
+	private int curIdx = -1;
+	
+	public PatternIterator(PatternElement pattern) {
+		this.pattern = pattern;
+		stack.push(pattern);
+	}
+	
+	public PatternElement getPattern() {
+		return pattern;
+	}
+	
+	public EndElement getNextEndElement() {
+		++curIdx;
+		while (curIdx < stack.size() && !(stack.get(curIdx) instanceof EndElement)) {
+			PatternElement cur = stack.get(curIdx);
+			if (cur instanceof WithChildsElement) {
+				stack.addAll(curIdx+1, ((WithChildsElement)cur).getChilds());
+			}
+			else { // Choice
+				stack.add(curIdx+1, ((Choice)cur).getNextChoice());
+			}
+			
+			++curIdx;
+		}
+		
+		if (curIdx < stack.size())
+			return (EndElement)stack.get(curIdx);
+		else
+			return null;
+	}
+	
+	public Couple<EndElement, EndElement> getLastChoice() {
+		EndElement lastEndElement = (EndElement)stack.get(curIdx);
+		--curIdx;
+		if (curIdx < 0)
+			return null;
+		PatternElement cur = stack.get(curIdx); 
+		while (curIdx >= 0) {
+			if (cur instanceof Choice) {
+				if (((Choice)cur).getNextChoice() == null)
+					((Choice)cur).resetChoice();
+				else {
+					stack.add(curIdx+1, ((Choice)cur).getCurChoice());
+					return new Couple<EndElement, EndElement>(lastEndElement,getNextEndElement());					
+				}
+			}
+			else if (cur instanceof EndElement) {
+				lastEndElement = (EndElement)cur;
+			}
+			
+			--curIdx;
+		}
+		
+		return null;
+	}
+}
