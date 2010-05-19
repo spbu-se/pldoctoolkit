1f89e7cecbd1250681340ddece0301b31a96779c
klon.angela <klon.angela@5f699344-e928-0410-99ec-830c0de225ea>
2010-05-19 12:50:23 +0000

    this and previous revisions:corrected interfaces (not all)

--- trunk/org.spbu.pldoctoolkit.text/src/org/spbu/pldoctoolkit/dialogs/CreateNewDictDialog.java
+++ trunk/org.spbu.pldoctoolkit.text/src/org/spbu/pldoctoolkit/dialogs/CreateNewDictDialog.java
@@ -166,7 +166,7 @@ public class CreateNewDictDialog extends Dialog {
 	@Override
 	protected void configureShell(Shell newShell) {
 		super.configureShell(newShell);
-		Point size = new Point(400,230);
+		Point size = new Point(427,230);
 		newShell.setSize(size);
 		Shell p = getParentShell();
 		newShell.setLocation(p.getLocation().x + p.getSize().x / 2 - size.x / 2, p.getLocation().y + p.getSize().y / 2 - size.y / 2);
