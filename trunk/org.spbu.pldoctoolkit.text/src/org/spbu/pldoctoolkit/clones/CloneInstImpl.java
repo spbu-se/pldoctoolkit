98c8e1a66faf0436ccca9abb22c4a38a5a36b6e1
artem.shutak@gmail.com <artem.shutak@gmail.com@5f699344-e928-0410-99ec-830c0de225ea>
2012-04-17 16:53:20 +0000

    Fixed bug when clones searching

--- trunk/org.spbu.pldoctoolkit.text/src/org/spbu/pldoctoolkit/clones/CloneInstImpl.java
+++ trunk/org.spbu.pldoctoolkit.text/src/org/spbu/pldoctoolkit/clones/CloneInstImpl.java
@@ -3,7 +3,6 @@ package org.spbu.pldoctoolkit.clones;
 import java.util.StringTokenizer;
 
 import org.spbu.pldoctoolkit.parser.DRLLang.LangElem;
-import org.spbu.pldoctoolkit.refactor.PositionInDRL;
 import org.spbu.pldoctoolkit.refactor.PositionInText;
 
 public class CloneInstImpl implements ICloneInst {
@@ -25,7 +24,7 @@ public class CloneInstImpl implements ICloneInst {
 	public String getName() {
 		return toString();
 	}
-	
+
 	@Override
 	public String toString() {
 		return infEl.getDRLDocument().file.getFullPath() + ":"
@@ -47,13 +46,13 @@ public class CloneInstImpl implements ICloneInst {
 	private PositionInText extractCloneTextAndGetTrueEndPosit(
 			PositionInText localStartPosOfClone,
 			PositionInText localEndPosOfClone) {
-		String text = infEl.getTextRepresentation();
-		StringTokenizer lineTokenizer = new StringTokenizer(text, "\n");
-		StringBuffer rez = new StringBuffer();
+		StringTokenizer lineTok = new StringTokenizer(infEl
+				.getTextRepresentation(), "\n");
 		for (int i = 1; i < localStartPosOfClone.line; i++) {
-			lineTokenizer.nextToken();
+			lineTok.nextToken();
 		}
-		String firstCloneLine = lineTokenizer.nextToken();
+		StringBuilder rez = new StringBuilder();
+		String firstCloneLine = lineTok.nextToken();
 		String lastCloneLine = null;
 		if (localStartPosOfClone.line == localEndPosOfClone.line) {
 			rez.append(firstCloneLine.substring(
@@ -62,23 +61,27 @@ public class CloneInstImpl implements ICloneInst {
 			lastCloneLine = firstCloneLine;
 		} else {
 			rez.append(firstCloneLine
-					.substring(localStartPosOfClone.column - 1)
-					+ "\n");
-			for (int i = 1; i < localEndPosOfClone.line
-					- localStartPosOfClone.line; i++) {
-				rez.append(lineTokenizer.nextToken() + "\n");
+					.substring(localStartPosOfClone.column - 1));
+			for (int i = localStartPosOfClone.line; i < localEndPosOfClone.line; i++) {
+				rez.append(lineTok.nextToken() + "\n");
 			}
-			lastCloneLine = lineTokenizer.nextToken();
+			lastCloneLine = lineTok.nextToken();
 			rez.append(lastCloneLine
 					.substring(0, localEndPosOfClone.column - 1));
 		}
-		StringTokenizer tok = new StringTokenizer(lastCloneLine
-				.substring(localEndPosOfClone.column - 1), " _,.");
-		String lastToken = tok.nextToken();
+		String lastToken = extractFirstToken(lastCloneLine
+				.substring(localEndPosOfClone.column - 1));
 		rez.append(lastToken);
 		cloneText = rez.toString();
 		return new PositionInText(localEndPosOfClone.line,
 				localEndPosOfClone.column + lastToken.length());
+	}
+
+	private String extractFirstToken(String string) {
+		StringTokenizer tokenTok = new StringTokenizer(string, " _,.");
+		if (!tokenTok.hasMoreTokens())
+			return "";
+		return tokenTok.nextToken();
 	}
 
 	@Override
@@ -88,50 +91,53 @@ public class CloneInstImpl implements ICloneInst {
 
 	@Override
 	public int getAbsoluteStartPosition() {
-		return convertPositionInTextToIntPosition(getAbsoluteStartPos(), getFullTextOfFile());
+		return convertPositionInTextToIntPosition(getAbsoluteStartPos(),
+				getFullTextOfFile());
 	}
 
-	private int convertPositionInTextToIntPosition(
-			PositionInText pos, String text) {
+	private int convertPositionInTextToIntPosition(PositionInText pos,
+			String text) {
 		int rez = 0;
 		StringTokenizer lineTok = new StringTokenizer(text, "\n");
 		for (int i = 1; i < pos.line; i++) {
 			String line = lineTok.nextToken();
-			rez+=line.length()+1;
+			rez += line.length() + 1;
 		}
-		rez+=pos.column;
+		rez += pos.column;
 		return rez;
 	}
 
 	@Override
 	public int getAbsoluteEndPosition() {
-		return convertPositionInTextToIntPosition(getAbsoluteEndPos(), getFullTextOfFile());
+		return convertPositionInTextToIntPosition(getAbsoluteEndPos(),
+				getFullTextOfFile());
 	}
-	
-	private PositionInText getAbsoluteStartPos(){
+
+	private PositionInText getAbsoluteStartPos() {
 		return getAbsolutePosition(infEl.getTagStartPos(), localStartPosOfClone);
 	}
-	
-	private PositionInText getAbsoluteEndPos(){
+
+	private PositionInText getAbsoluteEndPos() {
 		return getAbsolutePosition(infEl.getTagStartPos(), localEndPosOfClone);
 	}
-	
-	private PositionInText getAbsolutePosition(PositionInText prefixPos, PositionInText localPos){
-		int line = localPos.line+prefixPos.line;
+
+	private PositionInText getAbsolutePosition(PositionInText prefixPos,
+			PositionInText localPos) {
+		int line = localPos.line + prefixPos.line;
 		int column = localPos.column;
-		if (localPos.line==1)
-			column+=prefixPos.column;
-		StringTokenizer lineTok = new StringTokenizer(getFullTextOfFile(),"\n");
+		if (localPos.line == 1)
+			column += prefixPos.column;
+		StringTokenizer lineTok = new StringTokenizer(getFullTextOfFile(), "\n");
 		for (int i = 1; i < prefixPos.line; i++) {
 			lineTok.nextToken();
 		}
 		String lastPrefixLine = lineTok.nextToken();
-		if(prefixPos.column<lastPrefixLine.length())
+		if (prefixPos.column < lastPrefixLine.length())
 			line--;
 		return new PositionInText(line, column);
 	}
-	
-	private String getFullTextOfFile(){
+
+	private String getFullTextOfFile() {
 		return infEl.getDRLDocument().getTextRepresentation();
 	}
 
