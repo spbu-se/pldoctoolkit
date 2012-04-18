96fdfbbfeb5e8486072d3a7081d849e5a6e13201
artem.shutak <artem.shutak@gmail.com>
2012-04-18 12:09:37 +0000

    To class ClonesGroupResultView added popup menu (without functionality)

--- org.spbu.pldoctoolkit.text/src/org/spbu/pldoctoolkit/clones/view/ClonesGroupResultView.java
+++ org.spbu.pldoctoolkit.text/src/org/spbu/pldoctoolkit/clones/view/ClonesGroupResultView.java
@@ -11,20 +11,31 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.action.IContributionItem;
+import org.eclipse.jface.action.IMenuListener;
+import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.IToolBarManager;
 import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.action.Separator;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.DoubleClickEvent;
 import org.eclipse.jface.viewers.IDoubleClickListener;
+import org.eclipse.jface.viewers.IOpenListener;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.ISelectionProvider;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.ITreeViewerListener;
+import org.eclipse.jface.viewers.OpenEvent;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.jface.viewers.TreeExpansionEvent;
 import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.swt.events.HelpEvent;
+import org.eclipse.swt.events.HelpListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Menu;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.ide.IDE;
 import org.eclipse.ui.part.ViewPart;
@@ -37,6 +48,7 @@ public class ClonesGroupResultView extends ViewPart {
 
 	@Override
 	public void createPartControl(Composite parent) {
+		
 		GridLayout layout = new GridLayout();
 		layout.numColumns = 1;
 		layout.verticalSpacing = 2;
@@ -73,12 +85,41 @@ public class ClonesGroupResultView extends ViewPart {
 				doubleClickAction.run();
 			}
 		});
-//		MenuManager menuManager = new MenuManager("Lololololol DocLine");
-//		ISelectionProvider selectionProvider = new ISelectionProvider(){
-//
-//		getViewSite().registerContextMenu(menuManager, selectionProvider);
-//		}		
-		//treeViewer.expandAll();
+		
+		createPopupMenu();
+	}
+
+	private void createPopupMenu() {
+		MenuManager menuMgr = new MenuManager();
+		Menu menu = menuMgr.createContextMenu(treeViewer.getControl());
+		menuMgr.addMenuListener(new MyMenuListener());
+		menuMgr.setRemoveAllWhenShown(true);
+		treeViewer.getControl().setMenu(menu);
+	}
+	
+	private final class MyMenuListener implements IMenuListener {
+		@Override
+		public void menuAboutToShow(IMenuManager manager) {
+			ISelection iSelection = treeViewer.getSelection();
+			if ( ! iSelection.isEmpty() && iSelection instanceof IStructuredSelection){
+				IStructuredSelection selection = (IStructuredSelection) iSelection;
+				if (selection.getFirstElement() instanceof IClonesGroup){
+					manager.add(new ExtractAsNewInfElem("Extract as new InfElem"));						
+				}
+			}
+		}
+	}
+	
+	private final static class ExtractAsNewInfElem extends Action{
+		
+		ExtractAsNewInfElem(String text){
+			super(text);
+		}
+		
+		public void run() {
+			System.out.println("Ruuuun");
+		}
+		
 	}
 
 	@Override
