e90269c54162cf110a31e9289defca6c37ef0653
Dmitry V. Luciv <dluciv@dluciv.name>
2016-09-18 23:18:31 +0300

    More self-contained SFX

--- doc-clone-miner/build_dist/dcm-pack-windows.sh
+++ doc-clone-miner/build_dist/dcm-pack-windows.sh
@@ -6,13 +6,15 @@ COMP_LEVEL=9
 SFX_MODULE=
 # --------------
 
+python_subdir=${PYTHON_DIR##*\\}
+cp *.dll $PYTHON_DIR
+
 pushd ..
 
-python_subdir=${PYTHON_DIR##*\\}
 archive_name="doc-clone-miner-$(date +%Y-%m-%d).exe"
 git ls-files > dcm_files.lst
 
-echo 'start "" "%~dp0${python_subdir}\pythonw.exe" "%~dp0element_miner_ui.py" -ct "%~dp0clone_miner\clones.exe" %*' >run.cmd
+echo "start \"\" \"%~dp0${python_subdir}\\pythonw.exe\" \"%~dp0element_miner_ui.py\" -ct \"%~dp0clone_miner\\clones.exe\" %*" >run.cmd
 
 7z a build_dist\\${archive_name} -sfxbuild_dist\\7z.sfx -mx=${COMP_LEVEL} -ms=on -i@dcm_files.lst run.cmd -xr!build_dist ${PYTHON_DIR}
 
