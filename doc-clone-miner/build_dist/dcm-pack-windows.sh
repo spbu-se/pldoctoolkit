ea09a924698e35cdf19086ed7f45a5097d7c5fbe
Dmitry V. Luciv <dluciv@dluciv.name>
2016-09-18 21:21:55 +0300

    SFX correction

--- doc-clone-miner/build_dist/dcm-pack-windows.sh
+++ doc-clone-miner/build_dist/dcm-pack-windows.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 # -- settings --
-PYTHON_DIR=d:\\Python3
+PYTHON_DIR=d:\\Python34
 COMP_LEVEL=9
 SFX_MODULE=
 # --------------
 
 pushd ..
 
+python_subdir=${PYTHON_DIR##*\\}
 archive_name="doc-clone-miner-$(date +%Y-%m-%d).exe"
 git ls-files > dcm_files.lst
 
-echo 'start "" "%~dp0Python3\pythonw.exe" "%~dp0element_miner_ui.py" -ct "%~dp0clone_miner\clones.exe" %*' >run.cmd
+echo 'start "" "%~dp0${python_subdir}\pythonw.exe" "%~dp0element_miner_ui.py" -ct "%~dp0clone_miner\clones.exe" %*' >run.cmd
 
 7z a build_dist\\${archive_name} -sfxbuild_dist\\7z.sfx -mx=${COMP_LEVEL} -ms=on -i@dcm_files.lst run.cmd -xr!build_dist ${PYTHON_DIR}
 
