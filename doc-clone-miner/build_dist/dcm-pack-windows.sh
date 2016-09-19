d68fc0cf466fee371d16fc38732f67cb858140d4
Dmitry V. Luciv <dluciv@dluciv.name>
2016-09-19 09:43:18 +0300

    VC redist running after extraction

--- doc-clone-miner/build_dist/dcm-pack-windows.sh
+++ doc-clone-miner/build_dist/dcm-pack-windows.sh
@@ -15,10 +15,9 @@ pushd ..
 archive_name="doc-clone-miner-$(date +%Y-%m-%d).exe"
 git ls-files > dcm_files.lst
 
-echo "start \"\" \"%~dp0${python_subdir}\\pythonw.exe\" \"%~dp0element_miner_ui.py\" -ct \"%~dp0clone_miner\\clones.exe\" %*" >run.cmd
+cp build_dist/run.cmd1 run.cmd
+echo "start \"\" \"%~dp0${python_subdir}\\pythonw.exe\" \"%~dp0element_miner_ui.py\" -ct \"%~dp0clone_miner\\clones.exe\" %*" >>run.cmd
 
-cat build_dist/7zSD.sfx build_dist/7z.sfr > build_dist/7z.fsfx
-
-7z a build_dist\\${archive_name} -sfxbuild_dist\\7z.fsfx -mx=${COMP_LEVEL} -ms=on -i@dcm_files.lst run.cmd vcredist_x86.exe -xr!build_dist ${PYTHON_DIR}
+7z a build_dist\\${archive_name} -sfxbuild_dist\\7z.sfx -mx=${COMP_LEVEL} -ms=on -i@dcm_files.lst run.cmd vcredist_x86.exe -xr!build_dist ${PYTHON_DIR}
 
 popd
