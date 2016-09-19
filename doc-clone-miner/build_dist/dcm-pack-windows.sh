487fba8acaf0034239587898b108d5ef11b07beb
Dmitry V. Luciv <dluciv@dluciv.name>
2016-09-19 09:17:05 +0300

    VC redist in SFX

--- doc-clone-miner/build_dist/dcm-pack-windows.sh
+++ doc-clone-miner/build_dist/dcm-pack-windows.sh
@@ -8,6 +8,7 @@ SFX_MODULE=
 
 python_subdir=${PYTHON_DIR##*\\}
 cp *.dll $PYTHON_DIR
+cp vcredist_x86.exe ..
 
 pushd ..
 
@@ -16,6 +17,8 @@ git ls-files > dcm_files.lst
 
 echo "start \"\" \"%~dp0${python_subdir}\\pythonw.exe\" \"%~dp0element_miner_ui.py\" -ct \"%~dp0clone_miner\\clones.exe\" %*" >run.cmd
 
-7z a build_dist\\${archive_name} -sfxbuild_dist\\7z.sfx -mx=${COMP_LEVEL} -ms=on -i@dcm_files.lst run.cmd -xr!build_dist ${PYTHON_DIR}
+cat build_dist/7zSD.sfx build_dist/7z.sfr > build_dist/7z.fsfx
+
+7z a build_dist\\${archive_name} -sfxbuild_dist\\7z.fsfx -mx=${COMP_LEVEL} -ms=on -i@dcm_files.lst run.cmd vcredist_x86.exe -xr!build_dist ${PYTHON_DIR}
 
 popd
