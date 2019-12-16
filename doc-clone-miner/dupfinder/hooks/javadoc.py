3319d4ccfe38beb5912f0143996848ac5880560d
Dmitry V. Luciv <dluciv@dluciv.name>
2019-12-16 20:39:56 +0300

    Typo

--- doc-clone-miner/dupfinder/hooks/javadoc.py
+++ doc-clone-miner/dupfinder/hooks/javadoc.py
@@ -1,7 +1,7 @@
 from . import ReportHook
 
 import re
-_dogword = re.compile(r"(@(param|retrun|exception|deprecated|since|throw|version|author|see|since|serial))")
+_dogword = re.compile(r"(@(param|return|exception|deprecated|since|throw|version|author|see|since|serial))")
 
 class JavaDocHook(ReportHook):
     @staticmethod
